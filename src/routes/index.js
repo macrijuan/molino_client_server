4e5bdb4fb955a656f1c528a62fd65561a4ba940b
macrijuan <amacri162013@yahoo.com>
2024-01-13 02:56:11 -0300

    test3

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const { Dish } = require("../db.js").handler;
-// const public = require("./Client/index.js").handler;
-const test = require("./Test").handler;
+const public = require("./Client").handler;
 
 router.get("/", (req,res)=>{res.send('API status: OK')});
 
@@ -12,7 +11,6 @@ router.get("/test", async(req,res)=>{
   }).catch(err=>res.send(err));
 });
 
-// router.use("/public", public);
-router.use("/public", test);
+router.use("/public", public);
 
 module.exports.handler = router;
