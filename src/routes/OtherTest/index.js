faafa850342e214e30524cd4f0460f6e127aa996
macrijuan <amacri162013@yahoo.com>
2024-01-13 03:17:19 -0300

    test7

--- src/routes/OtherTest/index.js
+++ src/routes/OtherTest/index.js
@@ -1,7 +1,7 @@
 const {Router}=require("express");
 const router=Router();
 
-router.get("/asdf", async(req,res)=>{
+router.get("/", async(req,res)=>{
   res.json("HOLAAAA");
 });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 const { Dish } = require("../db.js").handler;
-const public = require("./Test").handler;
+// const public = require("./Test").handler;
+const test = require("./OtherTest");
 
 router.get("/", (req,res)=>{res.send('API status: OK')});
 
@@ -11,6 +12,6 @@ router.get("/test", async(req,res)=>{
   }).catch(err=>res.send(err));
 });
 
-router.use( "/testing", public );
+router.use( "/public", test.handler );
 
 module.exports.handler = router;
