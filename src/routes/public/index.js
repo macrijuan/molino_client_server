667720e39086618b6593c1f515c2a23eea8b3d0a
macrijuan <amacri162013@yahoo.com>
2024-01-13 02:36:06 -0300

    test1

--- src/routes/public/index.js
+++ src/routes/public/index.js
@@ -1,17 +1,20 @@
 const { Router } = require('express');
 const router = Router();
-const diet = require("./Diet/index.js").handler;
-const dish = require("./Dish/index.js").handler;
-const reservation = require("./Reservation/index.js").handler;
-const table = require("./Table/index.js").handler;
-const user = require("./User/index.js").handler;
+// const diet = require("./Diet/index.js").handler;
+// const dish = require("./Dish/index.js").handler;
+// const reservation = require("./Reservation/index.js").handler;
+// const table = require("./Table/index.js").handler;
+// const user = require("./User/index.js").handler;
 
-router.use(
-  ( req, res, next )=>{ 
-    if( Buffer.isBuffer(req.body) && req.body.length ) req.body = JSON.parse( req.body );
-    next(); 
-  },
-  diet, dish, reservation, table, user
-);
+// router.use(
+//   ( req, res, next )=>{ 
+//     if( Buffer.isBuffer(req.body) && req.body.length ) req.body = JSON.parse( req.body );
+//     next(); 
+//   },
+//   diet, dish, reservation, table, user
+// );
+router.get("/another_test", async(req,res)=>{
+  res.send("HOLAAAA");
+});
 
 module.exports.handler = router;
