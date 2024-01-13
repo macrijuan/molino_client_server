2ebb4cbdefb4d6a49ad66654e414b7ccfcf83c98
macrijuan <amacri162013@yahoo.com>
2024-01-13 04:08:15 -0300

    test23

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const test = require("./Test").handler;
+// const test = require("./Test").handler;
+
 const public = require("./Public").handler;
 
 router.get("/", (req,res)=>{res.send('API status: OK')});
 
-router.use( "/public", public, test );
+router.use( "/public", public );
 
 module.exports.handler = router;
