520d0a3b3908bbd3a109ed0f9b3981b84032f82b
macrijuan <amacri162013@yahoo.com>
2024-01-13 15:01:34 -0300

    test27

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 
-const public = require("./Public").handler;
+const public = require("./Z_Empty_Folder/index.js").handler;
 
 router.get("/", (req,res)=>{res.send('API status: OK')});
 
