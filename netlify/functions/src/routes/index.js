54027ec293445e14dc3032b59e4e223030460ba0
macrijuan <amacri162013@yahoo.com>
2024-01-14 01:49:50 -0300

    Delete necessary routes

--- netlify/functions/src/routes/index.js
+++ netlify/functions/src/routes/index.js
@@ -5,10 +5,6 @@ const t = require("./public/index.js");//common --> public --> Public
 
 router.get("/", (req,res)=>{res.send('API status: OK')});
 
-router.get("/test", async(req,res)=>{
-  Diet.findAll().then(diets=>{res.json(diets)}).catch(err=>{res.json({err:err})});
-});
-
 router.use("/public",t); //t
 
 module.exports = router;
