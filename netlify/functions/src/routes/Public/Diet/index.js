48c3b23e57c0a4c652403ce4dbbfaee460c8557e
macrijuan <amacri162013@yahoo.com>
2024-01-14 01:04:37 -0300

    TEST

--- netlify/functions/src/routes/Public/Diet/index.js
+++
@@ -1,7 +0,0 @@
-const {Router}=require("express");
-const router = Router();
-const getDiet = require("./Get/index.js");
-
-router.use("/diet", getDiet);
-
-module.exports = router;
--- netlify/functions/src/routes/Public/Table/index.js
+++
@@ -1,7 +0,0 @@
-const {Router}=require("express");
-const router = Router();
-const getTable=require("./Get/index.js");
-
-router.use("/table", getTable);
-
-module.exports = router;
