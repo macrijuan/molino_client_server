b3a319fa44920d13dac5e2f5642aa0d390ef0d24
macrijuan <amacri162013@yahoo.com>
2024-02-08 14:39:23 -0300

    test domain

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -10,7 +10,7 @@ const routes = require("./src/routes");
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://elmolinorestobar.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://elmolinorestobar');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
