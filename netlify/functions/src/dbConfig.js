6cbfd7fc7977293a5f3db2ff0eb7a3dcfe01a58b
macrijuan <amacri162013@yahoo.com>
2024-02-08 13:53:32 -0300

    Start tracking dbConfig.js

--- netlify/functions/src/dbConfig.js
+++ netlify/functions/src/dbConfig.js
@@ -1,3 +1,18 @@
 module.exports = {
-  logging: false, native:false
-}
+  dialectModule: pg,
+  logging: false,
+  native:false,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  },
+  pool: {
+    max: 2,
+    min: 0,
+    idle: 0,
+    acquire: 4000,
+    evict: process.env.CURRENT_LAMBDA_FUNCTION_TIMEOUT
+  }
+};
